9fdd209d74e6c0baccdaff1eff358033ba008261
Alex Coronell <alexcoronell@gmail.com>
2023-09-30 16:05:49 -0500

    Header basic content

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,22 @@
+import { Link } from "wouter";
+
+/* Styles */
+import './styles.scss';
+
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/experiences">Experiences</Link>
+        <Link href="/studies">Studies</Link>
+        <Link href="/training">Training</Link>
+        <Link href="/briefcase">Briefcase</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/login">Login</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
