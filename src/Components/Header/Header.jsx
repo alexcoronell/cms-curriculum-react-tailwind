5a42ca1b6f0cfe15cc2f69d280765dc2988cc4ab
Alex Coronell <alexcoronell@gmail.com>
2023-09-30 18:45:01 -0500

    Mobile menu partial completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,60 @@
 import { Link } from "wouter";
 
+import { MdMenu } from "react-icons/md";
+import {
+  AiOutlineClose,
+  AiOutlineGithub,
+  AiFillGitlab,
+  AiFillLinkedin,
+  AiOutlineMail,
+} from "react-icons/ai";
+
 /* Styles */
-import './styles.scss';
+import "./styles.scss";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/experiences">Experiences</Link>
-        <Link href="/studies">Studies</Link>
-        <Link href="/training">Training</Link>
-        <Link href="/briefcase">Briefcase</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/login">Login</Link>
+    <header className="Header">
+      <button className="Header__btn-menu">
+        <MdMenu />
+      </button>
+      <nav className="Header__nav">
+        <div className="Header__nav__close-btn-area">
+          <button className="Header__nav__close-btn-area__btn-close">
+            <AiOutlineClose />
+          </button>
+        </div>
+        {/* Menu */}
+        <ul className="Header__nav__ul">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/experiences">Experiences</Link>
+          </li>
+          <li>
+            <Link href="/studies">Studies</Link>
+          </li>
+          <li>
+            <Link href="/training">Training</Link>
+          </li>
+          <li>
+            <Link href="/briefcase">Briefcase</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+        </ul>
+
+        <div className="Header__nav__contact">
+          <a href="#" target="__blank" className="text-github"><AiOutlineGithub /></a>
+          <a href="#" target="__blank" className="text-gitlab"><AiFillGitlab /></a>
+          <a href="#" target="__blank" className="text-linkedin"><AiFillLinkedin /></a>
+          <a href="#" target="__blank" className="text-white"><AiOutlineMail /></a>
+        </div>
       </nav>
     </header>
   );
