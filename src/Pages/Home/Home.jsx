1414e62a464f5887afbb3f84e890e59d9f6f8cbc
Alex Coronell <43306609+alexcoronell@users.noreply.github.com>
2023-10-10 21:08:53 -0500

    Home page basic layout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,26 @@
+/* Images */
+import defaultUserImg from "../../assets/images/default-images/user.png";
+
+/* Styles */
+import './style.scss';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const name = "John Doe";
+  const title = "Fullstack developer";
+
+  return (
+    <div className="Home">
+      <main className="Home__contain">
+        <article className="Home__contain__imageBox">
+          <img src={defaultUserImg} alt={name + " - " + title + " image"} />
+        </article>
+        <article className="Home__contain__nameTitle">
+          <h1>{name}</h1>
+          <h2>{title}</h2>
+        </article>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
