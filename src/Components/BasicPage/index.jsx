929ecf1567eee23a7ccdce6091bf802847c3a179
Alex Coronell <43306609+alexcoronell@users.noreply.github.com>
2023-10-11 22:04:32 -0500

    Login page layout completed

---
+++ src/Components/BasicPage/index.jsx
@@ -0,0 +1,13 @@
+/* Style */
+import './style.scss';
+
+const BasicPage = (props) => {
+    console.log(props);
+    const { titlePage, children } = props;
+    return <div className="BasicPage">
+        <div className="BasicPage__titleArea"><h2>{titlePage}</h2></div>
+        <main className="BasicPage__content">{children}</main>
+    </div>
+}
+
+export default BasicPage;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,29 @@
+/* Components */
+import BasicPage from '../../Components/BasicPage';
+
+/* Styles */
+import './style.scss';
+
 const Login = () => {
-    return <h1>Login</h1>;
-  };
-  
-  export default Login;
-  
+  const titlePage = 'Login';
+
+  return <BasicPage titlePage={titlePage}>
+    <div className="Login content">
+      <div className="Login__box">
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" className='myTransition' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <button className='btn-primary w-full'>Login</button>
+        </form>
+      </div>
+    </div>
+  </BasicPage>
+};
+
+export default Login;
